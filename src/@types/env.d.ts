30b453dd81a394cd43646eafd8007f6dc3b234f5
Caique dos Santos <csdccarneiro@gmail.com>
2023-08-24 13:02:39 -0300

    Mudando ordem das variáveis de ambiente do GitHub

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,7 +1,7 @@
 /* TIPAGEM PARA VARIÁVEIS DE AMBIENTE DO ARQUIVO .ENV */
 
 declare module '@env' {
+    export const GITHUB_BASE_URL: string;
     export const GITHUB_CLIENT_ID: string;
     export const GITHUB_CLIENT_SECRET: string;
-    export const GITHUB_BASE_URL: string;
 }
