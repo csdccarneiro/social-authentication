afedd893f7f4a3edc89d0a5db25a1b7cc1d2aedc
Caique dos Santos <csdccarneiro@gmail.com>
2023-08-28 17:12:54 -0300

    Adicionando tipagem de variáveis de ambiente do twitter

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -5,4 +5,9 @@ declare module '@env' {
     export const GITHUB_API_BASE_URL: string;
     export const GITHUB_CLIENT_ID: string;
     export const GITHUB_CLIENT_SECRET: string;
+
+    export const TWITTER_BASE_URL: string;
+    export const TWITTER_API_BASE_URL: string;
+    export const TWITTER_CLIENT_ID: string;
+    export const TWITTER_CLIENT_SECRET: string;
 }
