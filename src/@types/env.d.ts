95a0b1665bded51c0bc063c5ee5bc291b9fc1c8f
Caique dos Santos <csdccarneiro@gmail.com>
2023-08-30 15:39:35 -0300

    Adicionando botão para autenticação social com o Spotify

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -10,4 +10,9 @@ declare module '@env' {
     export const TWITTER_API_BASE_URL: string;
     export const TWITTER_CLIENT_ID: string;
     export const TWITTER_CLIENT_SECRET: string;
+
+    export const SPOTIFY_BASE_URL: string;
+    export const SPOTIFY_API_BASE_URL: string;
+    export const SPOTIFY_CLIENT_ID: string;
+    export const SPOTIFY_CLIENT_SECRET: string;
 }
