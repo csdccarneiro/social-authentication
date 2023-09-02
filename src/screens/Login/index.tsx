92e17a6c305ccd98749ac1f29b84baa6644aed76
Caique dos Santos <csdccarneiro@gmail.com>
2023-09-02 19:54:51 -0300

    Estilizando botões e melhorando layout do app

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import * as WebBrowser from 'expo-web-browser';
 import { AuthRequest } from 'expo-auth-session';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import ConfigAuth from '../../config/auth';
 
 WebBrowser.maybeCompleteAuthSession();
@@ -23,12 +24,34 @@ export default function Login() {
 
   return (
     <View style={styles.container}>
-      <Text>Olá sou uma tela de login</Text>
-      <Button title="Login GitHub" color={'black'} onPress={() => requestAuth('github')} />
-      <Button title="Login Twitter" color={'blue'} onPress={() => requestAuth('twitter')} />
-      <Button title="Login Spotify" color={'green'} onPress={() => requestAuth('spotify')} />
-      <Button title="Login Reddit" color={'orange'} onPress={() => requestAuth('reddit')} />
-      <StatusBar style="auto" />
+      <View style={styles.containerText}>  
+        <Text style={styles.title}>
+          Bem-vindo ao Autenticador social
+        </Text>
+        <Text style={styles.description}>
+          Este app utiliza a autenticação do tipo OAuth e atualmente a mesma 
+          funciona com as plataformas abaixo:
+        </Text>
+      </View>
+      <View style={styles.containerButton}>
+        <FontAwesome.Button name="github" style={styles.button} backgroundColor="#000000" 
+          size={30} onPress={() => requestAuth('github')}>
+          Login com Github
+        </FontAwesome.Button>
+        <FontAwesome.Button name="twitter" style={styles.button} backgroundColor="#1DA1F2" 
+          size={30} onPress={() => requestAuth('twitter')}>
+          Login com Twitter
+        </FontAwesome.Button>
+        <FontAwesome.Button name="spotify" style={styles.button} backgroundColor="#1DB954" 
+          size={30} onPress={() => requestAuth('spotify')}>
+          Login com Spotify
+        </FontAwesome.Button>
+        <FontAwesome.Button name="reddit" style={styles.button} backgroundColor="#ff6314" 
+          size={30} onPress={() => requestAuth('reddit')}>
+          Login com Reddit
+        </FontAwesome.Button>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 
@@ -38,7 +61,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: '10%'
   },
+  containerText: {
+    marginBottom: 15
+  },
+  containerButton: {
+    height: '35%', 
+    justifyContent: 'space-evenly'
+  },
+  title: {    
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+  description: {    
+    fontSize: 14,
+    textAlign: 'center'
+  },
+  button: {    
+    justifyContent: 'center',
+    padding: 10
+  }
 });
