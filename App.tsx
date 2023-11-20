db0e42eb577f372df092cfec46b28032cfc1a695
Caique dos Santos <csdccarneiro@gmail.com>
2023-11-20 16:53:28 -0300

    Adicionando armazenamento de dados do usuário em local storage

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import * as React from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { Alert } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import * as WebBrowser from 'expo-web-browser';
 import { AuthRequest } from 'expo-auth-session';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import ConfigAuth from './src/config/auth';
 import Login from './src/screens/Login';
 import Profile from './src/screens/Profile';
@@ -11,21 +12,74 @@ WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
 
-  const [user, setUser] = React.useState<Global.UserProps>({})
+  const [user, setUser] = useState<Global.UserProps>({})
+
+
+  const saveUser = useCallback(async (value: Global.UserProps) => {
+
+    const jsonValue = JSON.stringify(value);
+        
+    await AsyncStorage.setItem('@user', jsonValue);
+
+    setUser(value)
+
+  }, [])
+
+
+  const getUser = useCallback(async () => {
+
+    const jsonStringValue = await AsyncStorage.getItem('@user') || JSON.stringify({});
+      
+    const jsonValue = JSON.parse(jsonStringValue);
+      
+    setUser(jsonValue)
+
+  }, [])
+
+
+  const removeUser = useCallback(async () => {
+
+    await AsyncStorage.removeItem('@user')
+
+    setUser({})
+
+  }, [])
+
+  useEffect(() => {
+
+    try {
+
+      getUser()
+    
+    } catch (err) {
+      
+      console.log(err)
+
+    }
+
+  }, [])
 
   async function requestLogin (platform: 'github' | 'twitter' | 'spotify' | 'reddit' | 'google' | 'facebook') {
 
-    const authPlatform = ConfigAuth[platform]
+    try {
 
-    const request = new AuthRequest(authPlatform.config);
+      const authPlatform = ConfigAuth[platform]
 
-    const response = await request.promptAsync(authPlatform.discovery);
+      const request = new AuthRequest(authPlatform.config);
 
-    const configToken = await authPlatform.getAccessToken(request, response)
+      const response = await request.promptAsync(authPlatform.discovery);
 
-    const currentUser = await authPlatform.getUser(configToken.access_token)
+      const configToken = await authPlatform.getAccessToken(request, response)
 
-    setUser(currentUser)
+      const currentUser = await authPlatform.getUser(configToken.access_token)
+
+      saveUser(currentUser)
+    
+    } catch (err) {
+
+      console.log(err)
+
+    }
 
   }
 
@@ -38,7 +92,19 @@ export default function App() {
       },
       {
         text: 'Ok', 
-        onPress: () => setUser({})
+        onPress: () => {
+
+          try {
+            
+            removeUser()
+
+          } catch(err) {
+
+            console.log(err)
+
+          }
+          
+        }
       }
     ]);
 
