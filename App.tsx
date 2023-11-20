df9c1b54522892d978beb3d0611448112e1701f0
Caique dos Santos <csdccarneiro@gmail.com>
2023-11-20 14:53:09 -0300

    Adicionando novamente tela de Login

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import * as WebBrowser from 'expo-web-browser';
 import { AuthRequest } from 'expo-auth-session';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import ConfigAuth from './src/config/auth';
+import Login from './src/screens/Login';
 import Profile from './src/screens/Profile';
 
 WebBrowser.maybeCompleteAuthSession();
@@ -31,81 +30,9 @@ export default function App() {
 
   return (
     <>
-      { 
-        !user.id ? 
-        <View style={styles.container}>
-          <View style={styles.containerText}>  
-            <Text style={styles.title}>
-              Bem-vindo ao Autenticador social
-            </Text>
-            <Text style={styles.description}>
-              Este app utiliza a autenticação do tipo OAuth e atualmente a mesma 
-              funciona com as plataformas abaixo:
-            </Text>
-          </View>
-          <View style={styles.containerButton}>
-            <FontAwesome.Button name="google" style={styles.button} backgroundColor="#d62d20" 
-              size={30} onPress={() => requestAuth('google')}>
-              Login com Google
-            </FontAwesome.Button>
-            <FontAwesome.Button name="github" style={styles.button} backgroundColor="#000000" 
-              size={30} onPress={() => requestAuth('github')}>
-              Login com Github
-            </FontAwesome.Button>
-            <FontAwesome.Button name="twitter" style={styles.button} backgroundColor="#1DA1F2" 
-              size={30} onPress={() => requestAuth('twitter')}>
-              Login com Twitter
-            </FontAwesome.Button>
-            <FontAwesome.Button name="spotify" style={styles.button} backgroundColor="#1DB954" 
-              size={30} onPress={() => requestAuth('spotify')}>
-              Login com Spotify
-            </FontAwesome.Button>
-            <FontAwesome.Button name="reddit" style={styles.button} backgroundColor="#ff6314" 
-              size={30} onPress={() => requestAuth('reddit')}>
-              Login com Reddit
-            </FontAwesome.Button>
-            <FontAwesome.Button name="facebook" style={styles.button} backgroundColor="#3b5998" 
-              size={30} onPress={() => requestAuth('facebook')}>
-              Login com Reddit
-            </FontAwesome.Button>
-            <StatusBar style="auto" />
-          </View>
-        </View> 
-         : 
-        <Profile user={user} /> 
-      }
+      <StatusBar style="auto" />
+      { !user.id ? <Login onAuth={requestAuth} /> : <Profile user={user} /> }
     </>
   );
 
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    paddingHorizontal: '10%'
-  },
-  containerText: {
-    marginBottom: 15
-  },
-  containerButton: {
-    height: '50%', 
-    justifyContent: 'space-evenly'
-  },
-  title: {    
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    textAlign: 'center'
-  },
-  description: {    
-    fontSize: 14,
-    textAlign: 'center'
-  },
-  button: {    
-    justifyContent: 'center',
-    padding: 13
-  }
-});
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+interface LoginProps {
+    onAuth: (platform: 'github' | 'twitter' | 'spotify' | 'reddit' | 'google' | 'facebook') => void
+}
+
+export default function Login({ onAuth }: LoginProps) {
+
+  return (
+    <View style={styles.container}>
+        <View style={styles.containerText}>  
+            <Text style={styles.title}>
+              Bem-vindo ao Autenticador social
+            </Text>
+            <Text style={styles.description}>
+              Este app utiliza a autenticação do tipo OAuth e atualmente a mesma 
+              funciona com as plataformas abaixo:
+            </Text>
+        </View>
+        <View style={styles.containerButton}>
+            <FontAwesome.Button name="google" style={styles.button} backgroundColor="#d62d20" 
+              size={30} onPress={() => onAuth('google')}>
+              Login com Google
+            </FontAwesome.Button>
+            <FontAwesome.Button name="github" style={styles.button} backgroundColor="#000000" 
+              size={30} onPress={() => onAuth('github')}>
+              Login com Github
+            </FontAwesome.Button>
+            <FontAwesome.Button name="twitter" style={styles.button} backgroundColor="#1DA1F2" 
+              size={30} onPress={() => onAuth('twitter')}>
+              Login com Twitter
+            </FontAwesome.Button>
+            <FontAwesome.Button name="spotify" style={styles.button} backgroundColor="#1DB954" 
+              size={30} onPress={() => onAuth('spotify')}>
+              Login com Spotify
+            </FontAwesome.Button>
+            <FontAwesome.Button name="reddit" style={styles.button} backgroundColor="#ff6314" 
+              size={30} onPress={() => onAuth('reddit')}>
+              Login com Reddit
+            </FontAwesome.Button>
+            <FontAwesome.Button name="facebook" style={styles.button} backgroundColor="#3b5998" 
+              size={30} onPress={() => onAuth('facebook')}>
+              Login com Facebook
+            </FontAwesome.Button>
+        </View>
+    </View>
+  );
+
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    paddingHorizontal: '10%'
+  },
+  containerText: {
+    marginBottom: 15
+  },
+  containerButton: {
+    height: '50%', 
+    justifyContent: 'space-evenly'
+  },
+  title: {    
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+  description: {    
+    fontSize: 14,
+    textAlign: 'center'
+  },
+  button: {    
+    justifyContent: 'center',
+    padding: 13
+  }
+});
